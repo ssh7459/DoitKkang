90f9de36bb7fd0722e7c154512cf010e0cb04372
ssh7459 <62632788+ssh7459@users.noreply.github.com>
2023-09-11 00:59:43 +0900

    ch11 내용

--- app/src/main/java/com/example/doitkkang/MainActivity.kt
+++ app/src/main/java/com/example/doitkkang/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.doitkkang
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.doitkkang.databinding.ActivityMainBinding
+import com.example.doitkkang.databinding.ItemMainBinding
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = this.toString()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val datas = mutableListOf<String>()
+        for (i in 1..10) {
+            datas.add("Item $i")
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = MyAdapter(datas)
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+    }
+}
+
+class MyViewHolder(val binding: ItemMainBinding): RecyclerView.ViewHolder(binding.root)
+
+class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val TAG = this.toString()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(ItemMainBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        Log.d(TAG, "onBindViewHolder: $position")
+        val binding = (holder as MyViewHolder).binding
+        // 뷰에 데이터 출력
+        binding.itemData.text = datas[position]
+        // 뷰에 이벤트 추가
+        binding.itemRoot.setOnClickListener {
+            Log.d(TAG, "item root click: $position")
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return datas.size
     }
 }
